232d5d47c457550fee5deb7ce180cb1117f0b35b
rafaelfs96 <rafaelfs96@outlook.com>
2021-02-22 17:50:09 -0300

    func basic 1

---
+++ chapter1-functional-programming/basic1.js
@@ -0,0 +1,20 @@
+//? function declaration
+function bomDia() {
+  console.log('bom dia')
+}
+bomDia()
+
+//? function expression
+const boaTarde = function () {
+  console.log('boa tarde')
+}
+boaTarde()
+
+//---------------------------------//
+
+function somar(a, b) {
+  return a + b
+}
+
+let result = somar(1, 2)
+console.log(result)
